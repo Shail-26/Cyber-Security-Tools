4da5ac51b844106394c483f4eaa6d06267179f90
Shail Macwan <119092769+Shail-26@users.noreply.github.com>
2024-03-17 14:48:31 +0530

    Add image encryption functionality

---
+++ scripts/image-encryption.js
@@ -0,0 +1,51 @@
+function showFileName(input) {
+    const fileName = input.files[0].name;
+    document.getElementById('file-name').textContent = 'Selected File: ' + fileName;
+}
+function showFileName1(input) {
+    const fileName1 = input.files[0].name;
+    document.getElementById('file-name1').textContent = 'Selected File: ' + fileName1;
+}
+
+function encryptImage() {
+    const imageInput = document.getElementById('imageInput');
+    const encryptionKey = document.getElementById('encryptionKey').value;
+    
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        const imageData = event.target.result.split(',')[1]; // Get base64 image data
+        
+        const encrypted = CryptoJS.AES.encrypt(imageData, encryptionKey).toString();
+        
+        const downloadEncryptedLink = document.getElementById('downloadEncryptedLink');
+        const downloadbtn = document.getElementById('download-btn');
+        downloadEncryptedLink.href = 'data:image/png;base64,' + encrypted;
+        downloadbtn.style.display = 'block';
+    };
+    
+    reader.readAsDataURL(imageInput.files[0]);
+}
+
+function decryptImage() {
+    const encryptedImageInput = document.getElementById('encryptedImageInput');
+    const decryptionKey = document.getElementById('decryptionKey').value;
+    
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        const encryptedImageData = event.target.result.split(',')[1]; // Get base64 image data
+        
+        const decrypted = CryptoJS.AES.decrypt(encryptedImageData, decryptionKey).toString(CryptoJS.enc.Utf8);
+        
+        const decryptedImage = document.getElementById('decryptedImage');
+        const decryptedImageDisplay = document.getElementById('decryptedImageDisplay');
+        decryptedImage.src = 'data:image/png;base64,' + decrypted;
+        decryptedImageDisplay.style.display = 'block';
+
+        const downloadDecryptedLink = document.getElementById('downloadDecryptedLink');
+        const downloadbtn = document.getElementById('download-btn-decrypt');
+        downloadDecryptedLink.href = 'data:image/png;base64,' + decrypted;
+        downloadbtn.style.display = 'block';
+    };
+    
+    reader.readAsDataURL(encryptedImageInput.files[0]);
+}
